95c449b74966ff5dc3947f740398bee7e3900ce6
truong280602 <86792928+truong280602@users.noreply.github.com>
2021-12-06 22:42:55 +0700

    nhom4 ver15

---
+++ Admin/Editproducts.php
@@ -0,0 +1,17 @@
+<?php
+include "config.php";
+include "models/db.php";
+include "models/product.php";
+$Product = new Product;
+if (isset($_POST['submit'])) {
+    $Product -> editProduct($_POST['id'], $_POST['name'], $_POST['manu_id'], $_POST['type_id'], $_POST['price'], $_FILES["image"]["name"], $_POST['description'], $_POST['feature']);
+    $target_dir = "../img/";
+    $target_file = $target_dir . basename($_FILES["image"]["name"]);
+    move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
+  }
+else
+{
+    echo 0;
+}
+header('location:products.php');
+?>
